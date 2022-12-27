a861008782870e9ab0bd1907cfc4e558cdbe4a09
Davide Troise <davide@troise.net>
2022-12-27 19:02:50 +0100

    Initial Commit

---
+++ ImageRenderingModeDemo/ImageRenderingModeDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  ImageRenderingModeDemoApp.swift
+//  ImageRenderingModeDemo
+//
+//  Created by Davide Troise with Swift 5.0
+//  for the YouTube channel "Coding con Davide" https://bit.ly/3QJziJE
+//
+
+import SwiftUI
+
+@main
+struct ImageRenderingModeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
